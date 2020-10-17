c856c0c562192a88aa2a0a4fcc201065af246648
Satoko Yamada <rotosux@gmail.com>
2020-10-17 13:20:33 +0900

    Updates

---
+++ precache-manifest.76f0fdf7d572bc187577c499b89cc215.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2898afa338f148341c941d45863ae620",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8790f851ad59c7691226",
+    "url": "/portfolio/static/css/main.a1dd0732.chunk.css"
+  },
+  {
+    "revision": "e2c46b647c7050d66931",
+    "url": "/portfolio/static/js/2.4a7fcc52.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/portfolio/static/js/2.4a7fcc52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8790f851ad59c7691226",
+    "url": "/portfolio/static/js/main.df85646b.chunk.js"
+  },
+  {
+    "revision": "31c73abe51dd1675e38e",
+    "url": "/portfolio/static/js/runtime-main.29d2e424.js"
+  },
+  {
+    "revision": "429eb930f1ab13763ec0b9902ea061eb",
+    "url": "/portfolio/static/media/bnr001.429eb930.jpg"
+  },
+  {
+    "revision": "7db81d9f5fa6da077ce4db3912959f7c",
+    "url": "/portfolio/static/media/bnr002.7db81d9f.jpg"
+  },
+  {
+    "revision": "31e792fb62b6970e58ae3bb2fc105f7e",
+    "url": "/portfolio/static/media/bnr003.31e792fb.jpg"
+  },
+  {
+    "revision": "6a0d153a981dfc2306adc30891aa475b",
+    "url": "/portfolio/static/media/cafeimg.6a0d153a.jpg"
+  },
+  {
+    "revision": "7128443d36a12980252d90dbeeebab66",
+    "url": "/portfolio/static/media/food_001.7128443d.jpg"
+  },
+  {
+    "revision": "31ef90cc24ca2768626a6a93182f8de5",
+    "url": "/portfolio/static/media/food_002.31ef90cc.jpg"
+  },
+  {
+    "revision": "2067d54991466366ec05e1f117226b42",
+    "url": "/portfolio/static/media/food_003.2067d549.jpg"
+  },
+  {
+    "revision": "7c72657f1cd3b3b6d2cfc5b06f4d8055",
+    "url": "/portfolio/static/media/goods_001.7c72657f.jpg"
+  },
+  {
+    "revision": "d5c57927684cc779361d0ab271fc15f8",
+    "url": "/portfolio/static/media/img_workbox01.d5c57927.jpg"
+  },
+  {
+    "revision": "34d87c5a8cb4a9cb0633682e28bff6f7",
+    "url": "/portfolio/static/media/img_workbox01s.34d87c5a.jpg"
+  },
+  {
+    "revision": "f88a4c8411048bc4747be681976fd410",
+    "url": "/portfolio/static/media/img_workbox02.f88a4c84.jpg"
+  },
+  {
+    "revision": "24ee457f186372514c138619448dfc7d",
+    "url": "/portfolio/static/media/img_workbox03.24ee457f.jpg"
+  },
+  {
+    "revision": "637072ec07eaa3d9bc59e97be1b3e950",
+    "url": "/portfolio/static/media/img_workbox04.637072ec.jpg"
+  },
+  {
+    "revision": "291854c14b90a7d0a98945a8da8aef56",
+    "url": "/portfolio/static/media/kanagawamap.291854c1.jpg"
+  },
+  {
+    "revision": "8710efa687a1613106bc77866c300da2",
+    "url": "/portfolio/static/media/plan_001.8710efa6.jpg"
+  },
+  {
+    "revision": "70cbae237e333e50af7de38a25c920b1",
+    "url": "/portfolio/static/media/plan_002.70cbae23.jpg"
+  },
+  {
+    "revision": "ef6f0e08a6b2e63fdaaedf7414621019",
+    "url": "/portfolio/static/media/plan_003.ef6f0e08.jpg"
+  },
+  {
+    "revision": "50959da115a4168d5227b41ec4a88269",
+    "url": "/portfolio/static/media/sugamomap.50959da1.jpg"
+  },
+  {
+    "revision": "8a82b649af2e9a53d524042e307f6425",
+    "url": "/portfolio/static/media/sweets_001.8a82b649.jpg"
+  },
+  {
+    "revision": "76ce5058ad13be0758175668031ae2be",
+    "url": "/portfolio/static/media/sweets_002.76ce5058.jpg"
+  },
+  {
+    "revision": "dfd97de071b022fa65a37b4a3e978a6f",
+    "url": "/portfolio/static/media/sweets_003.dfd97de0.jpg"
+  },
+  {
+    "revision": "b5bf39033d7af2fa078567f227bf61e2",
+    "url": "/portfolio/static/media/sweets_004.b5bf3903.jpg"
+  },
+  {
+    "revision": "4f4cd230856ee63ebb82e1813bb0a7e3",
+    "url": "/portfolio/static/media/sweets_005.4f4cd230.jpg"
+  },
+  {
+    "revision": "9bfd3c165982015e7abf8d93eb4b3e99",
+    "url": "/portfolio/static/media/sweets_006.9bfd3c16.jpg"
+  },
+  {
+    "revision": "eaeb41f10e98d1146e1c8564ecdca3a8",
+    "url": "/portfolio/static/media/topa.eaeb41f1.jpg"
+  },
+  {
+    "revision": "80815a7d60e4a49f64e1cb914c8abff1",
+    "url": "/portfolio/static/media/topb2.80815a7d.jpg"
+  },
+  {
+    "revision": "7cf5343fec6eb6bf98295df072af2dfb",
+    "url": "/portfolio/static/media/topb3.7cf5343f.jpg"
+  }
+]);
